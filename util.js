bc2d3324a8610d8d5223ab327b52dece0f007fd0
Hunter <hmason55@gmail.com>
2021-08-16 00:15:36 -0700

    Update util.js

--- util.js
+++ util.js
@@ -1,9 +1,9 @@
-function getCommitHistory(url, block) {
+function getCommitHistory(url, block, count = 5) {
 	$.getJSON(url, function(data) {
 		var text = "";
-		for(var node in data) {
-			var date = moment(data[node]["commit"]["committer"]["date"]).fromNow();
-			var message = data[node]["commit"]["message"];
+		for(var i = 0; i < count; i++)) {
+			var date = moment(data[i]["commit"]["committer"]["date"]).fromNow();
+			var message = data[i]["commit"]["message"];
 			text += message + "<br/>" + date + "<br/><br/>";
 		}
 		$(block).html(text);
