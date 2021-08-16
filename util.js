ecaa4c414193cfaa99aaddf92fd39dcbdf670b0a
Hunter <hmason55@gmail.com>
2021-08-16 00:16:24 -0700

    Update util.js

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
 function getCommitHistory(url, block, count = 5) {
 	$.getJSON(url, function(data) {
 		var text = "";
-		for(var i = 0; i < count; i++)) {
+		for(var i = 0; i < count; i++) {
 			var date = moment(data[i]["commit"]["committer"]["date"]).fromNow();
 			var message = data[i]["commit"]["message"];
 			text += message + "<br/>" + date + "<br/><br/>";
