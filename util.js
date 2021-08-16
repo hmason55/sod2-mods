503707df7db396eb915d85263356c35be8660200
Hunter <hmason55@gmail.com>
2021-08-15 19:36:43 -0700

    Update util.js

--- util.js
+++ util.js
@@ -1,7 +1,3 @@
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-<script>
 function getCommitHistory(url)
 {
 	$.getJSON(url, function(data) {
@@ -16,4 +12,3 @@ function getCommitHistory(url)
 
 	});
 }
-</script>
